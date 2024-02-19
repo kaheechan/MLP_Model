084db79752c8ca7f1628c6523fcf1560b47d0e05
Kahee Chan <121649764+kaheechan@users.noreply.github.com>
2024-02-18 22:26:46 -0800

    Add files via upload

--- MyProject/script/stage_3_script/main.py
+++ MyProject/script/stage_3_script/main.py
@@ -4,28 +4,28 @@ import torch as th
 
 if __name__ == '__main__':
     # ORL
-    # dataset_loader_ORL = Dataset_Loader('ORL', '')
-    # dataset_loader_ORL.dataset_folder = '../../data/stage_3_data'
-    # dataset_loader_ORL.dataset_file = 'ORL'
-    #
-    # train_loader, validation_loader, test_loader = dataset_loader_ORL.load_data()
-    #
-    # device = th.device("cuda" if th.cuda.is_available() else "cpu")
-    # method_cnn = Method_CNN('ORL', '').to(device)
-    #
-    # method_cnn.train_data(train_loader, validation_loader, test_loader, device)
+    dataset_loader_ORL = Dataset_Loader('ORL', '')
+    dataset_loader_ORL.dataset_folder = '../../data/stage_3_data'
+    dataset_loader_ORL.dataset_file = 'ORL'
+
+    train_loader, validation_loader, test_loader = dataset_loader_ORL.load_data()
+
+    device = th.device("cuda" if th.cuda.is_available() else "cpu")
+    method_cnn = Method_CNN('ORL', '').to(device)
+
+    method_cnn.train_data(train_loader, validation_loader, test_loader, device)
 
     # MNIST
-    # dataset_loader_MINST = Dataset_Loader('MNIST', '')
-    # dataset_loader_MINST.dataset_folder = '../../data/stage_3_data'
-    # dataset_loader_MINST.dataset_file = 'MNIST'
-    #
-    # train_loader, validation_loader, test_loader = dataset_loader_MINST.load_data()
-    #
-    # device = th.device("cuda" if th.cuda.is_available() else "cpu")
-    # method_cnn = Method_CNN('MNIST', '').to(device)
-    #
-    # method_cnn.train_data(train_loader, validation_loader, test_loader, device)
+    dataset_loader_MINST = Dataset_Loader('MNIST', '')
+    dataset_loader_MINST.dataset_folder = '../../data/stage_3_data'
+    dataset_loader_MINST.dataset_file = 'MNIST'
+
+    train_loader, validation_loader, test_loader = dataset_loader_MINST.load_data()
+
+    device = th.device("cuda" if th.cuda.is_available() else "cpu")
+    method_cnn = Method_CNN('MNIST', '').to(device)
+
+    method_cnn.train_data(train_loader, validation_loader, test_loader, device)
 
     # CIFAR
     dataset_loader_MINST = Dataset_Loader('CIFAR', '')
